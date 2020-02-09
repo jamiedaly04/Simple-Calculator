d3419711852aa6bc0283a17150fa034a5a9e613b
Jamie Daly <dalyjamie0@gmail.com>
2020-02-09 20:03:25 +0000

    Initial Commit of Calculator App

---
+++ CalcFuntions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple_Calculator
+{
+    public class CalcFuntions
+    {
+        public static void CalculatorAdd(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine(result);
+        }
+
+        public static void CalculatorSubtract(int num1, int num2)
+        {
+            int result = num1 - num2;
+            Console.WriteLine(result);
+        }
+
+        public static void CalculatorMultiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+            Console.WriteLine(result);
+        }
+
+        public static void CalculatorDivide(int num1, int num2)
+        {
+            int result = num1 / num2;
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Simple_Calculator
+{
+    public class Calculator : CalcFuntions
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your operator:\nFor example - = * /");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("Your result is:");
+
+            switch (operation.Trim().ToLower())
+            {
+                case "add":
+                case "+":
+                    CalculatorAdd(num1, num2);
+                    break;
+                case "subtract":
+                case "-":
+                    CalculatorSubtract(num1, num2);
+                    break;
+                case "mulitply":
+                case "*":
+                    CalculatorMultiply(num1, num2);
+                    break;
+                case "divide":
+                case "/":
+                    CalculatorDivide(num1, num2);
+                    break;
+                default:
+                    throw new InvalidOperationException("Specified operation is not recognized.");
+                    
+            }
+
+        }
+
+    }
+}
